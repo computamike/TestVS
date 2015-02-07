ccfae25b458dc3ec780ffb80e294345440dfbd10
Mike Hingley <Computa_Mike@hotmail.com>
2015-02-07 10:09:41 +0000

    Initial Commit

---
+++ RazorPartialViews/Attributes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorPartialViews
+{
+    [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    public  class HelpText : Attribute
+    {
+        // See the attribute guidelines at 
+        //  http://go.microsoft.com/fwlink/?LinkId=85236
+        readonly string positionalString;
+
+        // This is a positional argument
+        public HelpText(string positionalString)
+        {
+            this.positionalString = positionalString;
+
+
+        }
+
+        public string PositionalString
+        {
+            get { return positionalString; }
+        }
+
+         
+    }
+
+    class Attributes
+    {
+    }
+}
---
+++ RazorPartialViews/Models/Question.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorPartialViews.Models
+{
+    public class Question
+    {
+        [HelpText("This is Help Text")]
+        [Required]
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        [HelpText("This is your date of birth.")]
+        public DateTime  Birthday { get; set; }
+        [UIHint("ZipCode")]
+        [DisplayName("Zippadee")]
+        public string  ZipCode{ get; set; }
+    }
+}
